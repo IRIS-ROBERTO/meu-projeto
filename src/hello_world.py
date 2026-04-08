6df6c4ac5927e068152c57edfd846727d8795d5a
IRIS ROBERTO S. FERREIRA <iris369.iart@gmail.com>
2026-04-08 14:09:13 -0300

    Implementa script simples Hello World com docstring.

---
+++ src/hello_world.py
@@ -0,0 +1,11 @@
+
+# src/hello_world.py
+
+"""A simple hello world script with docstring.""
+
+def hello_world():
+    """Prints 'Hello World!' to the console.
+
+    :return: None
+    """
+    print('Hello World!')
